25be0b3c437e1d47742a6df18bb3f333b8528dac
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-19 17:18:43 +0900

    [Add]8-2

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const app = new Vue({
   el: '#app',
   data: {
-    items: [
-      { id: 1, text: '月曜日' },
-      { id: 2, text: '火曜日' },
-      { id: 3, text: '水曜日' },
-      { id: 4, text: '木曜日' },
-      { id: 5, text: '金曜日' },
-    ]
-  }
+    counter: 0,
+  },
 });
