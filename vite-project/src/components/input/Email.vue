03b05a678325f1b9ab0c20b6d93545bcd41e1c3d
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-26 20:54:02 +0900

    [Add]11-1

---
+++ vite-project/src/components/input/Email.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="form-outline mb-4">
+    <input type="email" id="email" class="form-control form-control-lg" />
+    <label class="form-label" for="email">Email</label>
+  </div>
+</template>
+
+<style scoped>
+</style>
