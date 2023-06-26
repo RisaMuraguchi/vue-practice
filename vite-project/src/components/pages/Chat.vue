22409bb261bb9eb7d3789dac526ab97c2286ad84
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-26 21:52:29 +0900

    チャット画面途中

---
+++ vite-project/src/components/pages/Chat.vue
@@ -0,0 +1,26 @@
+<script>
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      user: {},
+      chat: [],
+      input: '',
+      displayName:''
+    })
+    return {
+      data
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="container">
+    チャット用ページ
+  </div>
+</template>
+
+<style scoped>
+</style>
