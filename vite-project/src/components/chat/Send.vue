469b971f0d8fde9d7a78c83c4864f71a06f087a3
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-01 19:31:37 +0900

    Add

--- vite-project/src/components/chat/Send.vue
+++ vite-project/src/components/chat/Send.vue
@@ -1,5 +1,21 @@
 <script setup>
+import { ref } from "vue";
 
+const emit = defineEmits(['sendMessage']);
+const input = ref('');
+const update = (value) => {
+  const messageObj = {
+    displayName: "自分さん",
+    uid: "jibun",
+    message: value,
+  }
+  emit('sendMessage', messageObj)
+}
+const doSend = () => {
+  if (input.value === '') return;
+  update(input.value);
+  input.value = '';
+}
 </script>
 
 <template>
@@ -8,10 +24,12 @@
       type="text"
       class="form-control"
       placeholder="メッセージを入力してください"
+      v-model="input"
     />
     <button
       class="btn btn-outline-secondary js-send-message"
       type="button"
+      @click="doSend()"
     >
       メッセージ送信
     </button>
